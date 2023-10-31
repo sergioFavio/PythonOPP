27ce354050fee8935fa7c9345c67f0953aed8047
sergioFavio <serginho61@gmail.com>
2023-10-31 10:09:48 -0400

    version 2023-10-31_02

--- cuenta.py
+++ cuenta.py
@@ -1,3 +1,7 @@
 class Cuenta:
-    def __init__(self):
+    def __init__(self, numero, titular, saldo, limite):
         print("Construyendo el objeto...{}".format(self))
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
