b5547b98ac24a2da28a04ebe8fa114a9e51d0e1d
sergioFavio <serginho61@gmail.com>
2023-11-06 20:17:02 -0400

    version 2023-11-06_03

--- cuenta.py
+++ cuenta.py
@@ -15,3 +15,7 @@ class Cuenta:
     def retirar(self, valor):
         self.__saldo -= valor
         
+    def transferir(self, valor):
+        origen.retirar(valor)
+        destino.depositar(valor)
+        
