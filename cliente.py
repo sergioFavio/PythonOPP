c771e4b9f2b399326a358b6cc3073234fa8678f0
sergioFavio <serginho61@gmail.com>
2023-11-07 14:13:40 -0400

    version 2023-11-07_04

--- cliente.py
+++ cliente.py
@@ -1,4 +1,9 @@
 class Cliente:
     def __init__(self, nombre):
-        self.nombre = nombre
+        self.__nombre = nombre
+        
+    @property
+    def nombre(self):
+        print('llamando a @property nombre()')
+        return self.__nombre.title()
         
