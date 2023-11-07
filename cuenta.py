6a867a72cfd89c9e323752270929590f4836933c
sergioFavio <serginho61@gmail.com>
2023-11-07 10:07:34 -0400

    version 2023-11-07_02

--- cuenta.py
+++ cuenta.py
@@ -19,3 +19,15 @@ class Cuenta:
         self.retirar(valor)
         destino.depositar(valor)
         
+    def get_saldo(self):
+        return self.__saldo
+
+    def get_titular(self):
+        return self.__titular
+
+    def get_limite(self):
+        return self.__limite
+
+    def set_limite(self, limite):
+        self.__limite = limite
+        
